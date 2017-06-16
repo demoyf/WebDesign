b9c19b1a7f7d0a0dd7f7c3add81e9101ea5e4aec
xyf <425746853@qq.com>
2017-06-16 17:48:50 +0800

    自己登陆评论可删除，不能自己回复自己

--- tp5/application/index/controller/BlogDetail.php
+++ tp5/application/index/controller/BlogDetail.php
@@ -16,7 +16,8 @@ use app\index\model\Comment\CommentModel;
 use app\index\model\Comment\Reply as ReplyModel;
 use app\index\model\Blog\Tag as TagModel;
 use think\View;
-
+use think\Cookie;
+use app\index\model\Personal\Info as InfoModel;
 class BlogDetail extends Controller
 {
     public function showBlogDetail(Request $request)
@@ -80,6 +81,26 @@ class BlogDetail extends Controller
         }
         $myBlog->tags = $result_tag;
         $view->assign("blog", $myBlog);
+        $isHas = false;
+        $phone = "";
+        $info = "";
+        if (Cookie::has('phone', 'xyf_')) {
+            $isHas = true;
+            $phone = Cookie::get('phone', 'xyf_');
+            $info = InfoModel::get(['phone'=>$phone]);
+            if ($info ==null) {
+                $isHas = false;
+            }
+        }
+        if ($isHas) {
+            $view->phone = $phone;
+            $view->nickname = $info['nickname'];
+            $view->isLogin = 1;
+        }else{
+            $view->phone = "404";
+            $view->nickname = "404";
+            $view->isLogin = 0;
+        }
         return $view->fetch();
     }
 
@@ -93,6 +114,16 @@ class BlogDetail extends Controller
         $youke = YoukeModel::get(['email' => $youke_email]);
         $youke_id = null;
         $isSuccess = true;
+        $isNotMySelf = false;
+        if (Cookie::has('phone', 'xyf_')) {
+            $user_phone = Cookie::get("phone", "xyf_");
+            if ($youke_email == $user_phone) {
+//                输入的可以是自己的邮箱
+                $isNotMySelf = false;
+            } else {
+                $isNotMySelf = true;
+            }
+         }
         if ($youke == null) {
             $newYouke = new YoukeModel();
             $newYouke->link = $youke_link;
@@ -107,8 +138,13 @@ class BlogDetail extends Controller
         } else {
             $isSuccess = true;
             $youke_id = $youke['id'];
+            if ($youke_id == 1) {
+                if ($isNotMySelf) {
+                    $isNotMySelf = true;
+                }
+            }
         }
-        if ($isSuccess) {
+        if ($isSuccess&&!$isNotMySelf) {
             $comment_model = new CommentModel();
             $comment_model->blog_id = $blog_id;
             $comment_model->comment_content = $comment_content;
@@ -121,6 +157,7 @@ class BlogDetail extends Controller
             }
         } else {
             echo "<script>window.location.href='../../showBlogDetail?blog_id=".$blog_id."'</script>";
+            echo 500;
         }
     }
 
@@ -139,6 +176,16 @@ class BlogDetail extends Controller
         $comment_id = $request->post('comment_id');
         $youke = YoukeModel::get(['email' => $email]);
         $isSuccess = true;
+        $isNotMySelf = false;
+        if (Cookie::has('phone', 'xyf_')) {
+            $user_phone = Cookie::get("phone", "xyf_");
+            if ($email == $user_phone) {
+//                输入的可以是自己的邮箱
+                $isNotMySelf = false;
+            } else {
+                $isNotMySelf = true;
+            }
+        }
         if ($youke == null) {
             $youke = new YoukeModel();
             $youke->nickname = "".$nickname."";
@@ -153,6 +200,15 @@ class BlogDetail extends Controller
         } else {
             $isSuccess = true;
             $user_id = $youke['id'];
+//            没登录却用的是我的邮箱
+            if ($user_id == 1) {
+                if ($isNotMySelf) {
+                    $isNotMySelf = true;
+                }
+            }
+            if ($user_id == $reply_user) {
+                $isSuccess = false;
+            }
         }
         $replyModel = new ReplyModel();
         $replyModel->comment_id = $comment_id;
@@ -160,7 +216,7 @@ class BlogDetail extends Controller
         $replyModel->user_id = $user_id;
         $replyModel->reply_to = $reply_user;
         $replyModel->reply_time = date("Y-m-d H:i:s", time());
-        if ($isSuccess) {
+        if ($isSuccess&&$isNotMySelf) {
             if ($replyModel->save()) {
                echo "<script>window.location.href='../../showBlogDetail?blog_id=".$blog_id."'</script>";
             } else {
@@ -168,6 +224,29 @@ class BlogDetail extends Controller
             }
         } else {
             echo "<script>window.location.href='../../showBlogDetail?blog_id=".$blog_id."'</script>";
+        }
+    }
+
+    public function deleteComment(Request $request)
+    {
+        $comment_id = $request->post('comment_id');
+        $delete_what = $request->post('delete_what');
+        if (Cookie::has("phone", "xyf_")) {
+           $email =  Cookie::get("phone", "xyf_");
+            if ($email == "425746853@qq.com") {
+                if ($delete_what == 1) {
+                    db('reply')->where(['comment_id' => $comment_id])->delete();
+                    db('comment')->where(['id' => $comment_id])->delete();
+                    echo 200;
+                } else {
+                    db("reply")->where(['id' => $comment_id])->delete();
+                    echo 200;
+                }
+            } else {
+                echo 500;
+            }
+        } else {
+            echo 500;
         }
     }
 }
